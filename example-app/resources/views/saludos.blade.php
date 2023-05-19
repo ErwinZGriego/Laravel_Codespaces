6d6ef6dd421f130ef82c3835c48d853ab55a39c5
ErwinZGriego <100334592+ErwinZGriego@users.noreply.github.com>
2023-05-19 00:51:54 +0000

    created laravel project

---
+++ example-app/resources/views/saludos.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <header>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </header>
+    <body>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Beijing_bouddhist_monk_2009_IMG_1486.JPG/1200px-Beijing_bouddhist_monk_2009_IMG_1486.JPG" width="30" height="30" class="d-inline-block align-top" alt="">
+            Naverwin
+        </a>
+    </nav>
+        <h1 style="text-align:center; color:blue"><strong>Hola Erwin!!!</saludos></h1>
+    </body>
+</html>
